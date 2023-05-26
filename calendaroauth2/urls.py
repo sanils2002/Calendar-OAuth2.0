80cae0b3bb08eadd0d12a6bdd999095778377424
sanils2002 <sattarkars45@gmail.com>
2023-05-26 17:32:05 +0530

    Addn Files

---
+++ calendaroauth2/urls.py
@@ -0,0 +1,27 @@
+"""calendaroauth2 URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/4.0/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+# urls.py
+
+from django.urls import path
+from . import views
+
+
+urlpatterns = [
+    path('v1/calendar/init/', views.GoogleCalendarInitView,
+         name='google_permission'),
+    path('v1/calendar/redirect/',
+         views.GoogleCalendarRedirectView, name='google_redirect')
+]
